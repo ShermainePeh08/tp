5ec71a2dd5f4d20396f34311bd4b1af3a10e0ea1
Ming Xuan <66899567+icyyolo@users.noreply.github.com>
2026-03-12 11:28:14 +0800

    Fix storage manager to have 3 params

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -16,15 +16,19 @@ import seedu.address.commons.util.StringUtil;
 import seedu.address.logic.Logic;
 import seedu.address.logic.LogicManager;
 import seedu.address.model.AddressBook;
+import seedu.address.model.Inventory;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.model.ReadOnlyInventory;
 import seedu.address.model.ReadOnlyUserPrefs;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.VendorVault;
 import seedu.address.model.util.SampleDataUtil;
 import seedu.address.storage.AddressBookStorage;
+import seedu.address.storage.InventoryStorage;
 import seedu.address.storage.JsonAddressBookStorage;
+import seedu.address.storage.JsonInventoryStorage;
 import seedu.address.storage.JsonUserPrefsStorage;
 import seedu.address.storage.Storage;
 import seedu.address.storage.StorageManager;
@@ -59,7 +63,8 @@ public class MainApp extends Application {
         UserPrefsStorage userPrefsStorage = new JsonUserPrefsStorage(config.getUserPrefsFilePath());
         UserPrefs userPrefs = initPrefs(userPrefsStorage);
         AddressBookStorage addressBookStorage = new JsonAddressBookStorage(userPrefs.getAddressBookFilePath());
-        storage = new StorageManager(addressBookStorage, userPrefsStorage);
+        InventoryStorage inventoryStorage = new JsonInventoryStorage(userPrefs.getProductsFilePath());
+        storage = new StorageManager(addressBookStorage, userPrefsStorage, inventoryStorage);
 
         model = initModelManager(storage, userPrefs);
 
@@ -91,7 +96,22 @@ public class MainApp extends Application {
             initialData = new AddressBook();
         }
 
-        VendorVault initialVault = new VendorVault(initialData, SampleDataUtil.getSampleInventory());
+        Optional<ReadOnlyInventory> inventoryOptional;
+        ReadOnlyInventory initialInventory;
+        try {
+            inventoryOptional = storage.readInventory();
+            if (!inventoryOptional.isPresent()) {
+                logger.info("Creating a new data file " + storage.getInventoryFilePath()
+                        + " populated with a sample Inventory.");
+            }
+            initialInventory = inventoryOptional.orElseGet(SampleDataUtil::getSampleInventory);
+        } catch (DataLoadingException e) {
+            logger.warning("Data file at " + storage.getInventoryFilePath() + " could not be loaded."
+                    + " Will be starting with an empty Inventory.");
+            initialInventory = new Inventory();
+        }
+
+        VendorVault initialVault = new VendorVault(initialData, initialInventory);
 
         ModelManager modelManager = new ModelManager(initialVault, userPrefs);
 
