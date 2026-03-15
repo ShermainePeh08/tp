9c6dac3301978c336d2e4d6b70e7659ab68d7432
Jolene <jolenechong7@gmail.com>
2026-03-15 08:36:38 +0800

    feat: Clear contact confirmation

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -97,7 +97,7 @@ public class AddressBookParser {
                 return new DeleteCommandParser().parse(arguments);
 
             case ClearCommand.COMMAND_WORD:
-                return new ClearCommand();
+                return new ClearCommandParser().parse(arguments);
 
             case UndoCommand.COMMAND_WORD:
                 return new UndoCommand();
