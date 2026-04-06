8fe61aa505b6f03807524896dc059efd41bbcf5b
Le Hua <l3hu4l1@gmail.com>
2026-04-06 20:29:40 +0800

    Update FindCommandParser.java

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -1,13 +1,16 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.Messages.MESSAGE_NON_PREFIX_BEFORE_PREFIX;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.NameAndTagMatchesPredicate;
 import seedu.address.model.person.NameContainsKeywordsScoredPredicate;
 import seedu.address.model.person.PersonTagContainsKeywordsPredicate;
 
@@ -15,8 +18,7 @@ import seedu.address.model.person.PersonTagContainsKeywordsPredicate;
  * Parses input arguments and creates a new FindCommand object
  */
 public class FindCommandParser implements Parser<FindCommand> {
-    private static final String TAG_MODE_FLAG = "-t";
-    private static final String ESCAPED_TAG_MODE_FLAG = "/-t";
+    private static final String WHITESPACE_REGEX = "\\s+";
 
     /**
      * Parses the given {@code String} of arguments in the context of the FindCommand
@@ -24,40 +26,39 @@ public class FindCommandParser implements Parser<FindCommand> {
      * @throws ParseException if the user input does not conform to the expected format
      */
     public FindCommand parse(String args) throws ParseException {
-        String trimmedArgs = args.trim();
-        if (trimmedArgs.isEmpty()) {
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(" " + args, PREFIX_NAME, PREFIX_TAG);
+
+        if (!argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(MESSAGE_NON_PREFIX_BEFORE_PREFIX + FindCommand.MESSAGE_USAGE);
+        }
+
+        List<String> nameKeywords = flattenKeywordValues(argMultimap.getAllValues(PREFIX_NAME));
+        List<String> tagKeywords = flattenKeywordValues(argMultimap.getAllValues(PREFIX_TAG));
+
+        if (nameKeywords.isEmpty() && tagKeywords.isEmpty()) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
         }
 
-        List<String> rawTokens = Arrays.asList(trimmedArgs.split("\\s+"));
-        boolean isTagMode = rawTokens.contains(TAG_MODE_FLAG);
-
-        if (isTagMode) {
-            List<String> tagKeywords = rawTokens.stream()
-                    .filter(token -> !TAG_MODE_FLAG.equals(token))
-                    .map(this::toKeyword)
-                    .collect(Collectors.toList());
-
-            if (tagKeywords.isEmpty()) {
-                throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
-            }
-
+        if (nameKeywords.isEmpty()) {
             return new FindCommand(new PersonTagContainsKeywordsPredicate(tagKeywords));
         }
 
-        List<String> nameKeywords = rawTokens.stream()
-                .map(this::toKeyword)
-                .collect(Collectors.toList());
+        if (tagKeywords.isEmpty()) {
+            return new FindCommand(new NameContainsKeywordsScoredPredicate(nameKeywords));
+        }
 
-        return new FindCommand(new NameContainsKeywordsScoredPredicate(nameKeywords));
+        NameContainsKeywordsScoredPredicate namePredicate = new NameContainsKeywordsScoredPredicate(nameKeywords);
+        PersonTagContainsKeywordsPredicate tagPredicate = new PersonTagContainsKeywordsPredicate(tagKeywords);
+        return new FindCommand(new NameAndTagMatchesPredicate(namePredicate, tagPredicate));
     }
 
-    private String toKeyword(String token) {
-        if (ESCAPED_TAG_MODE_FLAG.equals(token)) {
-            return TAG_MODE_FLAG;
-        }
-        return token;
+    private List<String> flattenKeywordValues(List<String> rawValues) {
+        return rawValues.stream()
+                .flatMap(value -> Stream.of(value.split(WHITESPACE_REGEX)))
+                .map(String::trim)
+                .filter(token -> !token.isEmpty())
+                .toList();
     }
 
 }
