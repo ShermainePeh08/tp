533f65a930b0c31b174661756c69d6b676b4b887
Le Hua <l3hu4l1@gmail.com>
2026-04-09 03:56:57 +0800

    Update VendorVaultConsistencyUtilTest.java

--- src/test/java/seedu/address/model/util/VendorVaultConsistencyUtilTest.java
+++ src/test/java/seedu/address/model/util/VendorVaultConsistencyUtilTest.java
@@ -10,7 +10,10 @@ import static seedu.address.model.util.VendorVaultConsistencyUtil.MESSAGE_UNKNOW
 import static seedu.address.model.util.VendorVaultConsistencyUtil.findUnknownVendorLinksFromJson;
 import static seedu.address.model.util.VendorVaultConsistencyUtil.validateOrThrow;
 import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalProducts.OIL;
+import static seedu.address.testutil.TypicalProducts.RICE;
 import static seedu.address.testutil.TypicalProducts.getTypicalInventory;
 
 import java.nio.file.Path;
@@ -38,22 +41,10 @@ public class VendorVaultConsistencyUtilTest {
     private static final Path MISSING_FILE = TEST_DATA_FOLDER.resolve("missingInventoryFile.json");
     private static final String SKU_UNKNOWN = "SKU-UNKNOWN";
     private static final String SKU_DUP = "SKU-DUP";
-    private static final String SKU_1001 = "SKU-1001";
-    private static final String SKU_KNOWN = "SKU-KNOWN";
-    private static final String SKU_NO_EMAIL = "SKU-NO-EMAIL";
-    private static final String NAME_UNLINKED_PRODUCT = "Unlinked Product";
-    private static final String NAME_FIRST = "First";
-    private static final String NAME_SECOND = "Second";
-    private static final String NAME_KNOWN_VENDOR_PRODUCT = "Known Vendor Product";
-    private static final String NAME_NO_VENDOR = "No Vendor";
-    private static final String QUANTITY_ONE = "1";
-    private static final String QUANTITY_TWO = "2";
-    private static final String QUANTITY_THREE = "3";
-    private static final String THRESHOLD_ZERO = "0";
-    private static final String THRESHOLD_ONE = "1";
     private static final String VENDOR_EMAIL_MISSING = "missing@example.com";
     private static final String VENDOR_EMAIL_NOT_IN_FILE = "not-in-file@example.com";
-    private static final String VENDOR_EMAIL_ALICE = "alice@example.com";
+    private static final String SKU_KNOWN = "SKU-KNOWN";
+    private static final String SKU_NO_EMAIL = "SKU-NO-EMAIL";
     private static final String LINE_REFERENCE_SINGLE = "line 6";
     private static final String LINE_REFERENCE_MULTIPLE = "lines 7, 13";
     private static final String ISSUE_UNKNOWN_VENDOR_WITH_LINE = "'missing@example.com' at line 13";
@@ -83,11 +74,8 @@ public class VendorVaultConsistencyUtilTest {
 
     @Test
     public void validateOrThrow_unknownVendorEmail_throwsIllegalValueException() {
-        Product unlinkedProduct = new ProductBuilder()
+        Product unlinkedProduct = new ProductBuilder(RICE)
                 .withIdentifier(SKU_UNKNOWN)
-                .withName(NAME_UNLINKED_PRODUCT)
-                .withQuantity(QUANTITY_ONE)
-                .withThreshold(THRESHOLD_ZERO)
                 .withVendorEmail(VENDOR_EMAIL_MISSING)
                 .build();
 
@@ -100,11 +88,8 @@ public class VendorVaultConsistencyUtilTest {
 
     @Test
     public void validateOrThrow_unknownVendorEmailWithSourcePath_includesLineNumbers() {
-        Product unlinkedProduct = new ProductBuilder()
+        Product unlinkedProduct = new ProductBuilder(RICE)
                 .withIdentifier(SKU_UNKNOWN)
-                .withName(NAME_UNLINKED_PRODUCT)
-                .withQuantity(QUANTITY_ONE)
-                .withThreshold(THRESHOLD_ZERO)
                 .withVendorEmail(VENDOR_EMAIL_MISSING)
                 .build();
 
@@ -132,17 +117,11 @@ public class VendorVaultConsistencyUtilTest {
 
     @Test
     public void validateOrThrow_duplicateIdentifier_throwsIllegalValueException() {
-        Product first = new ProductBuilder()
+        Product first = new ProductBuilder(RICE)
                 .withIdentifier(SKU_DUP)
-                .withName(NAME_FIRST)
-                .withQuantity(QUANTITY_ONE)
-                .withThreshold(THRESHOLD_ZERO)
                 .build();
-        Product second = new ProductBuilder()
+        Product second = new ProductBuilder(OIL)
                 .withIdentifier(SKU_DUP)
-                .withName(NAME_SECOND)
-                .withQuantity(QUANTITY_TWO)
-                .withThreshold(THRESHOLD_ZERO)
                 .build();
 
         // EP: Use test double to bypass model-level uniqueness guards.
@@ -154,17 +133,11 @@ public class VendorVaultConsistencyUtilTest {
 
     @Test
     public void validateOrThrow_duplicateIdentifierWithPathButNoLineMatch_throwsGenericDuplicateMessage() {
-        Product first = new ProductBuilder()
+        Product first = new ProductBuilder(RICE)
                 .withIdentifier(SKU_DUP)
-                .withName(NAME_FIRST)
-                .withQuantity(QUANTITY_ONE)
-                .withThreshold(THRESHOLD_ZERO)
                 .build();
-        Product second = new ProductBuilder()
+        Product second = new ProductBuilder(OIL)
                 .withIdentifier(SKU_DUP)
-                .withName(NAME_SECOND)
-                .withQuantity(QUANTITY_TWO)
-                .withThreshold(THRESHOLD_ZERO)
                 .build();
 
         ReadOnlyInventory inventory = inventoryWithProducts(first, second);
@@ -175,17 +148,11 @@ public class VendorVaultConsistencyUtilTest {
 
     @Test
     public void validateOrThrow_duplicateIdentifierWithMissingPath_throwsGenericDuplicateMessage() {
-        Product first = new ProductBuilder()
+        Product first = new ProductBuilder(RICE)
                 .withIdentifier(SKU_DUP)
-                .withName(NAME_FIRST)
-                .withQuantity(QUANTITY_ONE)
-                .withThreshold(THRESHOLD_ZERO)
                 .build();
-        Product second = new ProductBuilder()
+        Product second = new ProductBuilder(OIL)
                 .withIdentifier(SKU_DUP)
-                .withName(NAME_SECOND)
-                .withQuantity(QUANTITY_TWO)
-                .withThreshold(THRESHOLD_ZERO)
                 .build();
 
         ReadOnlyInventory inventory = inventoryWithProducts(first, second);
@@ -196,33 +163,24 @@ public class VendorVaultConsistencyUtilTest {
 
     @Test
     public void validateOrThrow_duplicateIdentifierWithLineMatch_throwsLineAwareDuplicateMessage() {
-        Product first = new ProductBuilder()
-                .withIdentifier(SKU_1001)
-                .withName(NAME_FIRST)
-                .withQuantity(QUANTITY_ONE)
-                .withThreshold(THRESHOLD_ZERO)
+        Product first = new ProductBuilder(RICE)
                 .build();
-        Product second = new ProductBuilder()
-                .withIdentifier(SKU_1001)
-                .withName(NAME_SECOND)
-                .withQuantity(QUANTITY_TWO)
-                .withThreshold(THRESHOLD_ZERO)
+        Product second = new ProductBuilder(OIL)
+                .withIdentifier(RICE.getIdentifier().value)
                 .build();
 
         ReadOnlyInventory inventory = inventoryWithProducts(first, second);
 
         assertThrows(IllegalValueException.class,
-                String.format(MESSAGE_DUPLICATE_PRODUCT_IDENTIFIER_WITH_LINES, SKU_1001, LINE_REFERENCE_MULTIPLE), () ->
+                String.format(MESSAGE_DUPLICATE_PRODUCT_IDENTIFIER_WITH_LINES,
+                        RICE.getIdentifier().value, LINE_REFERENCE_MULTIPLE), () ->
                         validateOrThrow(getTypicalAddressBook(), inventory, DUPLICATE_IDENTIFIER_FILE));
     }
 
     @Test
     public void validateOrThrow_unknownVendorEmailWithPathButNoLineMatch_throwsIdentifierBasedMessage() {
-        Product unlinkedProduct = new ProductBuilder()
+        Product unlinkedProduct = new ProductBuilder(RICE)
                 .withIdentifier(SKU_UNKNOWN)
-                .withName(NAME_UNLINKED_PRODUCT)
-                .withQuantity(QUANTITY_ONE)
-                .withThreshold(THRESHOLD_ZERO)
                 .withVendorEmail(VENDOR_EMAIL_NOT_IN_FILE)
                 .build();
 
@@ -235,12 +193,9 @@ public class VendorVaultConsistencyUtilTest {
 
     @Test
     public void validateOrThrow_knownVendorEmailWithSourcePath_success() {
-        Product linkedProduct = new ProductBuilder()
+        Product linkedProduct = new ProductBuilder(RICE)
                 .withIdentifier(SKU_KNOWN)
-                .withName(NAME_KNOWN_VENDOR_PRODUCT)
-                .withQuantity(QUANTITY_ONE)
-                .withThreshold(THRESHOLD_ZERO)
-                .withVendorEmail(VENDOR_EMAIL_ALICE)
+                .withVendorEmail(ALICE.getEmail().value)
                 .build();
 
         ReadOnlyInventory inventory = inventoryWithProducts(linkedProduct);
@@ -250,11 +205,8 @@ public class VendorVaultConsistencyUtilTest {
 
     @Test
     public void validateOrThrow_withoutVendorEmail_success() {
-        Product productWithoutVendor = new ProductBuilder()
+        Product productWithoutVendor = new ProductBuilder(RICE)
                 .withIdentifier(SKU_NO_EMAIL)
-                .withName(NAME_NO_VENDOR)
-                .withQuantity(QUANTITY_THREE)
-                .withThreshold(THRESHOLD_ONE)
                 .withoutVendorEmail()
                 .build();
 
