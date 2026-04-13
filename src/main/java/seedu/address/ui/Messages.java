aca03778d779633de8dbb597a5db347ab77018ab
Le Hua <l3hu4l1@gmail.com>
2026-04-13 21:40:13 +0800

    Update Messages.java

--- src/main/java/seedu/address/ui/Messages.java
+++ src/main/java/seedu/address/ui/Messages.java
@@ -5,10 +5,12 @@ package seedu.address.ui;
  */
 public final class Messages {
 
-    public static final String DUPLICATE_IDENTIFIER_PREFIX = "Duplicate product identifier";
     public static final String MESSAGE_DATA_FILE_AT = "Data file at ";
+    public static final String MESSAGE_COULD_NOT_READ_DATA_IN = "Could not read data in ";
     public static final String MESSAGE_CREATING_NEW_DATA_FILE = "Creating a new data file ";
     public static final String MESSAGE_ILLEGAL_VALUES_FOUND_IN = "Illegal value(s) found in ";
+    public static final String MESSAGE_INVALID_JSON_FORMAT = "Invalid JSON format";
+    public static final String MESSAGE_INVALID_JSON_FORMAT_AT_LINE = "Invalid JSON format at line ";
     public static final String MESSAGE_LOG_SEPARATOR = ": ";
     public static final String MESSAGE_COULD_NOT_LOAD_STARTING_EMPTY_ADDRESS_BOOK =
             " could not be loaded. Starting with empty AddressBook.";
@@ -22,7 +24,6 @@ public final class Messages {
             " populated with a sample Inventory.";
     public static final String MESSAGE_POPULATED_EMPTY_ALIAS_FILE =
             " populated with an empty Alias file.";
-    public static final String MESSAGE_UNKNOWN_VENDOR_EMAIL = "Unknown vendor email ";
     public static final String NEWLINE = "\n";
 
     private Messages() {}
